eadd2322f34af4151f3f0f50685a3c5ebaa65607
lisa <lbthoms@mail.fhsu.edu>
2023-09-25 00:18:13 -0600

    Updated readme file to show more information on the project

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 ### Lisa Thoms
 ### Mini Project 02
 
+![Picture of BTS, the biggest K-POP act in the world. ](https://people.com/thmb/sQSl8jHALt3PIvnpVV2uUWxB1ew=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(599x0:601x2)/BTS_JOIN_LOUIS_VUITTON_AS_HOUSE_AMBASSADORS-a525d86a4d4745dd8ff413cc723c6c7e.jpg)
+
 ## Description
-Mini Project 2 takes a data set (K-Pop Hits from 2021) and creates meaningful graphs to describe the data.
+Mini Project 2 takes a data set (Top 50 K-Pop Hits from 2021) and creates meaningful graphs to describe the data.
+
+The graphs generated will print information regarding:
+
+* Male versus Female acts
+* Tempo versus Danceability 
+* Key Type: Major versus Minor
+* Speechiness versus Track Length
+* Valence versus Loudness
 
  ## Pip Install Instructions
 Please run the following:
--- main.py
+++ main.py
@@ -3,33 +3,31 @@
 # Mini Project 2
 
 import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
 
-# (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or
-# retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
-# Think of some question you would like to solve such as:
-# "How many homes in the US have access to 100Mbps Internet or more?"
-# "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
-# Here are some other great datasets: https://www.kaggle.com/datasets
-
+# import dataset of KPop Hits 2021
+# https://www.kaggle.com/datasets/sberj127/kpop-hits-through-the-years?select=KPopHits2021.csv
+# added one addition column - gender
 data = pd.read_csv(filepath_or_buffer='KPopHits2021.csv', index_col='artist/s')
 
 print(data.head())
 
-# (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is
-# labeled tabular data.
 
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build
 # some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 
-# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder,
-# the project should save these when it executes. You may want to add this folder to your .gitignore file.
 
-# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+# Start of program
+try:
+    # Create our charts folder
+    Path("charts").mkdir()
+except FileExistsError:
+    pass
 
-# (10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt
-# file which contains all the packages that need installed. You can create this fille with the output of pip freeze at
-# the terminal prompt.
+# Saves plot
+savefile = "charts/" + ".png"
+plt.savefig(savefile)
 
-# (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the
-# pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
-# Be thorough on the explanations.
+# Shows graph
+plt.show()
